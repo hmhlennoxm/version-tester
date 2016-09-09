d2d8ea8d1e0f0668e08ae960d5fcbe972f92a5cb
Mark Lennox <mark.lennox@hmhco.com>
2016-09-09 12:20:23 +0100

    tweaking workflow

--- bump.js
+++ bump.js
@@ -75,18 +75,36 @@ const bump_version = (ver_cmd) => {
   let sync_tag = (ver_cmd !== package_cmd)
 
   let bump_handler = (cmd) => {
-    console.log('cmd : ', cmd)
     exec_promise('npm version ' + cmd + ' -m "' + cmd + ' bumped tag/version to %s"', { error_message: 'Could not bump version' })
       .then(exec_promise('git push --follow-tags', { error_message: 'Could not push version and tags to git' } ))  
-  }  
+  }
+
+  console.log('cmd : ', ver_cmd)
 
   if (sync_tag) {
-    exec_promise('npm version from-git', { error_message: 'Could not fetch latest tag from git' } )
-      .then(bump_handler(ver_cmd))
+    console.log('yes, sync_tag if we can')
+    package_version_usable(process.env.npm_package_version)
+      // .then(() => {
+      //   console.log('then hit')
+      // })
+      // .catch(() => {
+      //   console.error('catch hit')
+      // })
+      // if the local package version matches the latest tag then running 'npm version from-git' throws an error
+      .then(() => {
+        console.log('then 2 hit')
+        exec_promise('npm version from-git', { error_message: 'Could not fetch latest tag from git' } )
+      })
+      .catch(() => {
+        console.error('catch 2 hit')
+        bump_handler(ver_cmd)
+          .catch((err) => console.log('catch 3 : ', err))
+      })
   } else {
+    console.log('no, we are providing tag')
     package_version_usable(process.env.npm_package_version)
       .then((package_version) => bump_handler('"' + package_version + '"'))
-      .catch((err) => console.log(err))
+      .catch((err) => console.log('branch 2 error caught : ', err))
   }
 
 
@@ -118,24 +136,26 @@ const package_version_usable = function(package_version) {
       reject('Version specified in package.json is not valid semver : ' + package_version)
     }
 
-    exec('git describe --tags `git rev-list --tags --max-count=1 --remotes`', (error, latest_tag, stderr) => {
+    exec('git describe --tags --abbrev=0 `git rev-list --tags --max-count=1 --remotes`', (error, latest_tag, stderr) => {
       latest_tag = latest_tag.trim()
         
       let is_usable = semver.gt(package_version, latest_tag)
       console.log('will update and tag git repo from "' + latest_tag + '" to "' + package_version + '"? :', is_usable)
 
       if (is_usable){
+        console.error('it is usable - resolving')
         resolve(package_version)
       } else {
+        console.error('it is NOT usable - rejecting!!')
         reject('The proposed version "' + package_version + '" must be greater than ther existing latest tag "' + latest_tag + '"')
       }
     })
   })
 }
 
+console.log(argv._)
 
-const ver_cmd = argv._ ? argv._[0] : 'patch'
-
+const ver_cmd = (argv._ && argv._.length > 0) ? argv._[0] : 'patch'
 
 if (commands.indexOf(ver_cmd) > -1){
   bump_version(ver_cmd)
